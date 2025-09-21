895bf15f33ac60d6dce121c3530ab05483a1ca62
kanha-beep <kanhashree2223@gmail.com>
2025-09-21 07:30:29 +0530

    Update client env setup and axios config

--- client/src/init/api.js
+++ client/src/init/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // replace with your backend URL
+  baseURL: import.meta.env.VITE_API_URL, // replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
